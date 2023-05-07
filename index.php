4091505bc6e334809c294228ca5db81e7f09b487
chukwu kelvin <kelvinchukwueke26@gmail.com>
2023-05-07 22:23:39 +0100

    new update

--- index.php
+++ index.php
@@ -1,20 +1,41 @@
 <?php
-// get form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
 
-// set email recipient and subject
-$to = 'ayooluwasamuel619@gmail.com';
-$subject = 'New message from contact form';
+// Check if form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-// set email headers
-$headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n";
-$headers .= 'Reply-To: ' . $email . "\r\n";
-$headers .= 'Content-type: text/plain; charset=UTF-8' . "\r\n";
+  // Collect form data
+    $title = test_input($_POST["title"]);
+    $fullname = test_input($_POST["fullname"]);
+    $phonenumber = test_input($_POST["phonenumber"]);
+    $subject = test_input($_POST["subject"]);
+    $infoname = test_input($_POST["infoname"]);
+    $email = test_input($_POST["email"]);
+    $message = test_input($_POST["message"]);
 
-// send email
-mail($to, $subject, $message, $headers);
 
-echo 'Message sent successfully!';
+  // Set recipient email address
+    $to = 'ayooluwasamuel619@gmail.com';
+    $subject = 'New message from Church website';
+
+  // Set email headers
+    $headers = "From: $email" . "\r\n" .
+            "Reply-To: $email" . "\r\n" .
+            "X-Mailer: PHP/" . phpversion();
+
+  // Send email
+    mail($to, $subject, $message, $headers);
+
+  // Redirect to thank-you page
+    echo 'Message sent successfully thank you!';
+exit;
+}
+
+// Function to sanitize input data
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+return $data;
+    }
+
 ?>
--- js/index.js
+++ js/index.js
@@ -1,29 +1,71 @@
-const form = document.getElementById('index.php');
+// const form = document.getElementById('Hayah-section');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault(); // prevent form submission
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault(); // prevent form submission
 
-  // validate form input
-  const name = document.getElementById('name').value.trim();
-  const email = document.getElementById('email').value.trim();
+//   // validate form input
+//   const title = document.getElementById('name').value.trim();
+//   const fullname = document.getElementById('email').value.trim();
+//   const phonenumber = document.getElementById('message').value.trim();
+//   const subject  = document.getElementById('name').value.trim();
+//   const infoname = document.getElementById('email').value.trim();
+//   const message = document.getElementById('message').value.trim();
+//   const email = document.getElementById('email').value.trim();
+
+//   if (!title || !fullname || !phonenumber || ! subject || !infoname || !message || !email) {
+//     alert('Please fill in all fields');
+//     return;
+//   }
+
+//   // send form data to PHP script
+//   const xhr = new XMLHttpRequest();
+//   const url = 'index.php';
+//   const data = new FormData(form);
+
+//   xhr.open('POST', url, true);
+//   xhr.onreadystatechange = function() {
+//     if (xhr.readyState === 4 && xhr.status === 200) {
+//       alert(xhr.responseText);
+//       form.reset(); // reset form after successful submission
+//     }
+//   };
+//   xhr.send(data);
+// });
+
+
+const form = document.getElementById('info-section'); // 'contact-section' Hayah-section
+const submitBtn = document.getElementById('submit-btn');
+
+submitBtn.addEventListener('click', function(e) {
+  e.preventDefault(); // prevent default form submission action
+
+  // get values of form inputs
+  const title = document.getElementById('name').value.trim();
+  const fullname = document.getElementById('email').value.trim();
+  const phonenumber = document.getElementById('message').value.trim();
+  const subject  = document.getElementById('name').value.trim();
+  const infoname = document.getElementById('email').value.trim();
   const message = document.getElementById('message').value.trim();
+  const email = document.getElementById('email').value.trim();
 
-  if (!name || !email || !message) {
-    alert('Please fill in all fields');
-    return;
-  }
+  // store values in an object
+  const formData = { title, fullname, phonenumber, subject, infoname, message, email };
 
-  // send form data to PHP script
-  const xhr = new XMLHttpRequest();
-  const url = 'send-email.php';
-  const data = new FormData(form);
-
-  xhr.open('POST', url, true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      alert(xhr.responseText);
-      form.reset(); // reset form after successful submission
+  // send data to server-side script using AJAX or fetch
+  // e.g. using fetch
+  fetch('index.php', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log('Form data submitted successfully');
+    } else {
+      console.log('Error submitting form data');
     }
-  };
-  xhr.send(data);
+  })
+  .catch(error => {
+    console.log('Error submitting form data:', error);
+  });
 });
